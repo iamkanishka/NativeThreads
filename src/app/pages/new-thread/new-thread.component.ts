17ab4f9fa37ece2cc61dc68e7c221fd42c941f01
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-07 22:12:19 +0530

    NewThreadComponent

---
+++ src/app/pages/new-thread/new-thread.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-thread',
+  imports: [],
+  templateUrl: './new-thread.component.html',
+  styleUrl: './new-thread.component.scss'
+})
+export class NewThreadComponent {
+
+}
