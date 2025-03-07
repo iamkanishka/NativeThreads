02fe5c1b281733997d909f69fee635be4541e8d7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-07 22:12:41 +0530

    NewThreadModule

---
+++ src/app/pages/new-thread/new-thread.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NewThreadRoutingModule } from './new-thread-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NewThreadRoutingModule
+  ]
+})
+export class NewThreadModule { }
