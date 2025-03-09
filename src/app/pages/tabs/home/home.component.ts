7cec094403cf3864669f48f7f2ab002fe38dcd57
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-09 21:27:37 +0530

    chore: Hide action bar

--- src/app/pages/tabs/home/home.component.ts
+++ src/app/pages/tabs/home/home.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core'
-
-import { DataService, DataItem } from '../shared/data.service'
+import { Component, OnInit } from "@angular/core";
+import { Page } from "@nativescript/core";
 
 @Component({
-  selector: 'Home',
-  templateUrl: './home.component.html',
+  selector: "Home",
+  templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
-  items: Array<DataItem>
-
-  constructor(private _itemService: DataService) {}
-
-  ngOnInit(): void {
-    this.items = this._itemService.getItems()
+  constructor(private _page: Page) {
+    this._page.actionBarHidden = true;
   }
+
+  ngOnInit(): void {}
 }
