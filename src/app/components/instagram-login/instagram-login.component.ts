d13d8774c3f98293a3a90e7355902035adf74927
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-10 21:23:59 +0530

    InstagramLoginComponent

---
+++ src/app/components/instagram-login/instagram-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instagram-login',
+  imports: [],
+  templateUrl: './instagram-login.component.html',
+  styleUrl: './instagram-login.component.scss'
+})
+export class InstagramLoginComponent {
+
+}
