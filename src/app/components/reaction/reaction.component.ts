897ddf3a43091cdb24650ab36fa47031decce715
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-10 21:22:49 +0530

    ReactionComponent

---
+++ src/app/components/reaction/reaction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reaction',
+  imports: [],
+  templateUrl: './reaction.component.html',
+  styleUrl: './reaction.component.scss'
+})
+export class ReactionComponent {
+
+}
