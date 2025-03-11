cada34dc16c1296a1938412c8819ce23442b6b2e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:23 +0530

    chore: Removed empty imports

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
