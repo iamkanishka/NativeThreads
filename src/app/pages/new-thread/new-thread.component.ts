a81a40b8c270d9c6c0bbaa9265018df95827576c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:41 +0530

    chore: Removed empty imports

--- src/app/pages/new-thread/new-thread.component.ts
+++ src/app/pages/new-thread/new-thread.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-thread',
-  imports: [],
+  
   templateUrl: './new-thread.component.html',
   styleUrl: './new-thread.component.scss'
 })
