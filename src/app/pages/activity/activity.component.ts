ac03409c5352ea43f40c417a63603b659af61b47
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:30 +0530

    chore: Removed empty imports

--- src/app/pages/activity/activity.component.ts
+++ src/app/pages/activity/activity.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-activity',
-  imports: [],
+  
   templateUrl: './activity.component.html',
   styleUrl: './activity.component.scss'
 })
