7255a39fb6f573a2fe8a8f9ebe6b12db66bcdb97
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:20 +0530

    chore: Removed empty imports

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
