d46724940aab1df17fcff45a57d6f5c7585c2f5e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:09 +0530

    chore: Removed empty imports

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-comments',
-  imports: [],
+  
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss'
 })
