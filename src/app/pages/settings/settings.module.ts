4dec5e76544bb0e43ceb0d4679e13ce5e704dc42
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:38:28 +0530

    chore: Update ActivityModule imports to use NativeScriptCommonModule and add NO_ERRORS_SCHEMA.

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
+ 
 import { SettingsRoutingModule } from './settings-routing.module';
+import { NativeScriptCommonModule } from '@nativescript/angular'
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
+    NativeScriptCommonModule,
     SettingsRoutingModule
-  ]
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class SettingsModule { }
