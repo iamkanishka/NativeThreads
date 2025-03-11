499110afeadfa73334e1b76496dc8a8036ef5cfd
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:46 +0530

    chore: Removed empty imports

--- src/app/pages/settings/privacy/privacy.component.ts
+++ src/app/pages/settings/privacy/privacy.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-privacy',
-  imports: [],
+  
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss'
 })
