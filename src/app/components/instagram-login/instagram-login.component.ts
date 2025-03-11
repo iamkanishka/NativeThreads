de51eac66ed76cba6c9acf0b2eb105041749fcd3
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:18 +0530

    chore: Removed empty imports

--- src/app/components/instagram-login/instagram-login.component.ts
+++ src/app/components/instagram-login/instagram-login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-instagram-login',
-  imports: [],
+  
   templateUrl: './instagram-login.component.html',
   styleUrl: './instagram-login.component.scss'
 })
