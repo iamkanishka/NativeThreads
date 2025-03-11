18c98e19ba802ac5c28cc114fdd703d0fca55117
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:34 +0530

    chore: Removed empty imports

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
