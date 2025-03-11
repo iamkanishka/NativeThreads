37141fdad244451ffff1fb70647ae62b6c9558cd
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:25 +0530

    chore: Removed empty imports

--- src/app/components/reaction/reaction.component.ts
+++ src/app/components/reaction/reaction.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reaction',
-  imports: [],
+  
   templateUrl: './reaction.component.html',
   styleUrl: './reaction.component.scss'
 })
