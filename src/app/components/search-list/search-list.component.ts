b6afe9f929aaf1bda5c2a05a200d9d6ab4d23ab7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:28 +0530

    chore: Removed empty imports

--- src/app/components/search-list/search-list.component.ts
+++ src/app/components/search-list/search-list.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-list',
-  imports: [],
+  
   templateUrl: './search-list.component.html',
   styleUrl: './search-list.component.scss'
 })
