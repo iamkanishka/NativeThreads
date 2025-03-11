5a768e115400e178cba7c329500a78eeb543a4bb
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:33:36 +0530

    chore: Removed empty imports

--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
