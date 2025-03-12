c90f005e5389c9e70a5491d8ee26993d571c5757
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-12 22:04:24 +0530

    Declared ActivityComponent

--- src/app/pages/activity/activity.module.ts
+++ src/app/pages/activity/activity.module.ts
@@ -2,9 +2,10 @@ import { NativeScriptCommonModule } from "@nativescript/angular";
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
 import { ActivityRoutingModule } from "./activity-routing.module";
+import { ActivityComponent } from "./activity.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [ActivityComponent],
   imports: [NativeScriptCommonModule, ActivityRoutingModule],
   schemas: [NO_ERRORS_SCHEMA]
 })
