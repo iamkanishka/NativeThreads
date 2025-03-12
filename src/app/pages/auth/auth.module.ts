1096ec7626e94861ab857299bea71a7bfe2230d8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-12 22:04:35 +0530

    Declared AuthComponent, SignInComponent, SignUpComponent

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
 import { AuthRoutingModule } from "./auth-routing.module";
 import { NativeScriptCommonModule } from "@nativescript/angular";
+import { AuthComponent } from "./auth.component";
+import { SignInComponent } from "./sign-in/sign-in.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [AuthComponent, SignInComponent, SignUpComponent],
   imports: [NativeScriptCommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
